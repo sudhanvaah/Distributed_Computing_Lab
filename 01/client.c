a0df7377f86e2b2e563cb1c98fdbfdcaea2c2184
Sudhanva <sudhanvaah@users.noreply.github.com>
2016-10-27 09:51:31 +0530

    .

--- 01/client.c
+++ 01/client.c
@@ -1,34 +1,23 @@
 #include<stdio.h>
-#include<sys/socket.h>
+#include<stdlib.h>
+#include<unistd.h>
 #include<sys/types.h>
-#include<netinet/in.h>
-void str_echo(int s)
+#include<sys/socket.h>
+#include<arpa/inet.h>
+void main()
 {
-char buf[50],buf1[50];
-puts("Enter the Message...");
-fgets(buf,50,stdin);
-send(s,buf,50,0); //sending data to server
-//receiving data from server
-recv(s,buf1,50,0);
-puts("Message from Server...");
-fputs(buf1,stdout);
-}
-int main()
-{
-int ls;
-struct sockaddr_in cli;
-puts("I am Client...");
-/*creating socket*/
-ls=socket(AF_INET,SOCK_STREAM,0);
-puts("Socket Created Successfully...");
-/*socket address structure*/
-cli.sin_family=AF_INET;
-cli.sin_addr.s_addr=inet_addr("127.0.0.1");
-cli.sin_port=htons(5000);
-/*connecting to server*/
-connect(ls,(struct sockaddr*)&cli,sizeof(cli));
-puts("Connected with Server...");
-str_echo(ls);
-close(ls);
-return 0;
+	int sfd;
+	char buf[50],buf1[50];
+	struct sockaddr_in sa;
+	sa.sin_family=AF_INET;
+	sa.sin_port=htons(18000);
+	sa.sin_addr.s_addr=inet_addr("127.0.0.1");
+	sfd=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+	connect(sfd,(struct sockaddr *)&sa,sizeof(sa));
+	puts("Enter data to send to client");
+	fgets(buf,50,stdin);
+	send(sfd,buf,sizeof(buf),0);
+	recv(sfd,buf1,sizeof(buf1),0);
+	fputs(buf1,stdout);
+	close(sfd);
 }
