0fcb84c7acb87dd6c9f916030628f4a4c000e402
Sudhanva <sudhanvaah@users.noreply.github.com>
2016-12-26 07:26:51 +0530

    .

---
+++ 09.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct process
+{
+	int processID;
+	bool crashed;
+};
+process processes[100];
+int totalprocess;
+int cordinator,initiator;
+void init();
+void ring(int);//actual logic function for this code
+int main()
+{
+	init();
+	while(true)
+	{
+		cout<<"\n1.Crash process \n 2. Recover Process\n";
+		int choice;
+		cout<<"Enter your choice?\n";
+		cin>>choice;
+		if(choice==1)
+		{
+			cout<<"Which process u want to crash?";
+			int p;
+			cin>>p;
+			processes[p].crashed=true;
+			if(p==initiator)
+			{
+				int newinitiator=0;
+				for(newinitiator=0;newinitiator<totalprocess;newinitiator++)
+				{
+					if(newinitiator!=initiator and processes[newinitiator].crashed==false)
+					{
+						initiator=newinitiator;
+						break;
+					}
+				}			
+			}
+			ring(initiator);
+		}
+		if(choice==2)
+		{
+			cout<<"Whoch process u want to recover:\n";
+			int p;
+			cin>>p;
+			processes[p].crashed=false;
+			ring(initiator);
+		}
+	}
+}
+void init()
+{
+	cout<<"Enter the no of process in the system\n";
+	cin>>totalprocess;
+	for(int i=0;i<totalprocess;++i)
+	{
+		processes[i].processID=i;
+		cout<<"Enter the process id of the process- "<<i<<": "<<i<<endl;
+		processes[i].crashed=false;
+	}
+	cout<<"Enter the initiator process no:\n";
+	cin>>initiator;
+	ring(initiator);
+}
+
+void ring(int processno)
+{
+	int uncrashed[100],count=0;
+	for(int i=0;i<totalprocess;++i)
+	{
+		if(processes[i].crashed==false)
+		{
+			uncrashed[count++]=i;
+		}
+	}
+	
+	if(count==1 and uncrashed[0]==processno)
+	{
+		cout<<"only process "<<processes[processno].processID<<" is left so coordinator is "<<processes[processno].processID<<endl;
+		cordinator=processno;
+	}
+	else if(count==1)
+	{
+		cout<<"only process active is "<<uncrashed[0]<<" so coodinator is "<<uncrashed[0]<<endl;
+		cordinator=uncrashed[0];
+	}
+	else if(count==0)
+	{
+		cout<<"No process is active\n";
+		cordinator=-1;
+	}
+	else
+	{
+		int currprocessno=0;
+		while(uncrashed[currprocessno]!=processno)
+			currprocessno++;
+		//this is getting the currprocessno same as initiator
+		do//starting the ring algo from initiator i.e processno
+		{
+			cout<<"\nMessage sent from "<<uncrashed[currprocessno]<< "to "<<uncrashed[(currprocessno+1)%count];
+			currprocessno=(currprocessno+1)%count;
+		}while(uncrashed[currprocessno]!=processno);
+		cordinator=*(max_element(uncrashed,uncrashed+count));
+		cout<<"\n New cordinator is : "<<cordinator<<endl;
+	}
+
+}
