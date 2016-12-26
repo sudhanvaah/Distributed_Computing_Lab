aca11dd9c0240e18261c855f1a59757d25876ce0
Sudhanva <sudhanvaah@users.noreply.github.com>
2016-12-26 07:08:00 +0530

    .

--- 07/keepalive.c
+++ 07/keepalive.c
@@ -7,18 +7,12 @@ int main(int argc, char **argv)
  socklen_t optlen;
 
  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
- if(sockfd == -1)
-     printf("Error");
 
  int res = 0;
 
 
  optlen = sizeof(optval);
  res = getsockopt(sockfd, SOL_SOCKET, SO_KEEPALIVE, &optval, &optlen);
-
- if(res == -1)
-     printf("Error getsockopt one");
- else
       printf("SO_KEEPALIVE is %s\n", (optval ? "ON" : "OFF"));
 
  // Set KeepLIVE
@@ -27,18 +21,11 @@ int main(int argc, char **argv)
  printf("sets the keepalive\n");
  res = setsockopt(sockfd, SOL_SOCKET, SO_KEEPALIVE, &optval, sizeof(optval));
 
- if(res == -1)
-     printf("Error setsockopt");
-
 
  // test now
-optval=0;
  optlen = sizeof(optval);
  res = getsockopt(sockfd, SOL_SOCKET, SO_KEEPALIVE, &optval, &optlen);
 
- if(res == -1)
-     printf("Error getsockopt two");
- else
      printf("SO_KEEPALIVE is %s\n", (optval ? "ON" : "OFF"));
 
  return 0;
